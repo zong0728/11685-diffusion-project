f61df2919b7c593106ef1b1e20bbb94a3d88b376
zong0728 <shengzhg@andrew.cmu.edu>
2026-04-16 03:25:16 -0400

    Fix VAE checkpoint loading under PyTorch 2.6+ (weights_only=True default)

--- models/vae.py
+++ models/vae.py
@@ -45,7 +45,7 @@ class VAE(nn.Module):
         return dec
 
     def init_from_ckpt(self, path, ignore_keys=list()):
-        sd = torch.load(path, map_location="cpu")["state_dict"]
+        sd = torch.load(path, map_location="cpu", weights_only=False)["state_dict"]
         keys = list(sd.keys())
         for k in keys:
             for ik in ignore_keys:
